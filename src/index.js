938dfa77f4a5ca5524e9ba456830726fec2fee67
Jimias <Jimias@metropolia.fi>
2025-01-22 16:57:27 +0200

    week2 branch

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import {getItems} from './items.js';
 const hostname = '127.0.0.1';
 const app = express();
 const port = 3000;
@@ -11,10 +12,13 @@ app.use(express.json());
 
 // rest-apin resurssit tarjoillaan /api/-polun alla
 app.get('/api/', (req, res) => {
-  console.log('get-pyyntö juureen havaittu');
+  console.log('get-pyyntö apin juureen havaittu');
   console.log(req.url);
   res.send('Welcome to my REST API!');
 });
+
+// Items resurssin päätepisteet (endpoint)
+app.get('/api/items', getItems);
 
 // syötteen lukeminen reittiparametreista (route params)
 app.get('/api/sum/:num1/:num2', (req, res) => {
@@ -55,6 +59,7 @@ app.post('/api/moro', (req, res) => {
   res.status(200);
   res.json({reply: 'no Moro ' + req.body.sender});
 });
+
 
 // TODO: lisää oma reitti ja toiminnallisuus omaa mielikuvitusta käyttäen, niin
 app.get('/api/greet/:name', (req, res) => {
