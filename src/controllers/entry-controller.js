2e53316b9b1235f5ca6d927ea11c96dfc8437b58
Jimias <Jimias@metropolia.fi>
2025-03-10 19:17:20 +0200

    uudet muutokset

--- src/controllers/entry-controller.js
+++ src/controllers/entry-controller.js
@@ -1,16 +1,14 @@
 import {insertEntry, selectEntriesByUserId} from '../models/entry-model.js';
 
-const postEntry = async (req, res) => {
+const postEntry = async (req, res, next) => {
   // user_id, entry_date, mood, weight, sleep_hours, notes
-  // todo add trycatch
+  const newEntry = req.body;
+  newEntry.user_id = req.user.user_id;
   try {
-    const newEntry = req.body;
-    newEntry.user_id = req.user.user_id;
-    insertEntry(newEntry);
+    await insertEntry(newEntry);
     res.status(201).json({message: "Entry added."});
   } catch (error) {
-    console.error("Error adding entry:", error);
-    res.status(500).json({message: "Failed to add entry. Please try again later."});
+    next(error);
   }
 };
 
@@ -19,15 +17,13 @@ const postEntry = async (req, res) => {
  * @param {*} req
  * @param {*} res
  */
-const getEntries = async (req, res) => {
+const getEntries = async (req, res, next) => {
   try {
     const entries = await selectEntriesByUserId(req.user.user_id);
     res.json(entries);
   } catch (error) {
-    console.error("Error fetching entries:", error);
-    res.status(500).json({message: "Failed to retrieve entries. Please try again later."});
+    next(error);
   }
 };
 
 export {postEntry, getEntries};
-
